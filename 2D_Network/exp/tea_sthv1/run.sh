7c219229bb37a4c65039f163cd0f9da5613ef239
BeSpontaneous <41779062+BeSpontaneous@users.noreply.github.com>
2023-04-21 10:17:55 -0400

    exp_2d

---
+++ 2D_Network/exp/tea_sthv1/run.sh
@@ -0,0 +1,11 @@
+
+#### scripts for training TEA on Something-Something V1
+### v=16
+
+CUDA_VISIBLE_DEVICES=0,1 python main.py something RGB \
+     --arch_file resnet_TEA \
+     --arch resnet50 --num_segments 16 \
+     --amp --gd 20 --lr 0.02 --lr_steps 30 40 45 --epochs 50 \
+     --batch-size 32 -j 16 --dropout 0.5 --consensus_type=avg --eval-freq=1 \
+     --model_path 'models' \
+     --shift --shift_div=8 --shift_place=blockres --npb --round 1;
---
+++ 2D_Network/exp/tsm_sthv1_FFN/run.sh
@@ -0,0 +1,11 @@
+
+#### scripts for training TSM_FFN on Something-Something V1
+### v_h=16, v_m=8, v_l=4
+
+CUDA_VISIBLE_DEVICES=0,1 python main_FFN.py something RGB \
+     --arch_file resnet_TSM_FFN \
+     --arch resnet50 --num_segments_H 16 --num_segments_M 8 --num_segments_L 4 \
+     --amp --gd 20 --lr 0.01 --lr_steps 20 40 --epochs 50 \
+     --batch-size 32 -j 16 --dropout 0.5 --consensus_type=avg --eval-freq=1 \
+     --lambda_act 1 --model_path 'models_FFN' \
+     --shift --shift_div=8 --shift_place=blockres --npb --round 1;
